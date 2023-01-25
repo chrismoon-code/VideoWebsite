361cb71dbf18544d17675809b645a5d6e85aa1ca
Christopher Moon <77298032+chrismoon-code@users.noreply.github.com>
2023-01-25 08:37:27 +0000

    First Commit

---
+++ website.js
@@ -0,0 +1,16 @@
+'use strict';
+const switcher = document.querySelector('.btn');
+switcher.addEventListener('click',function(){
+    document.body.classList.toggle('light-theme');
+    document.body.classList.toggle('dark-theme');
+
+const classname = document.body.className;
+if(classname = "light-theme"){
+   this.textContent = "Dark";
+}
+else
+{
+    this.textContent = "Light"
+}
+console.log('current classname' + classname);
+});
