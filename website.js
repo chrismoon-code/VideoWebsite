971fe805c3dfffe67bbcf2299ed68d5fa3ee9d81
Christopher Moon <77298032+chrismoon-code@users.noreply.github.com>
2023-02-16 00:08:04 +0000

    added pictures, reformatted site, added buttons

--- website.js
+++ website.js
@@ -1,15 +1,18 @@
-'use strict';
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click',function(){
-    document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
-const classname = document.body.className;
-if(classname = "light-theme"){
-   this.textContent = "Dark";
+    document.body.classList.toggle('cyan-theme');
+if(document.body.classList.contains('cyan-theme')){
+   this.innerText = "Dark Mode";
 }
 else
 {
-    this.textContent = "Light"
+    this.innerText = "Cyan Mode";
 }
-console.log('current classname' + classname);
+});
+document.querySelector('.github').addEventListener('click',function(){
+    location.href = "https://github.com/chrismoon-code"
+});
+document.querySelector('.LinkedIn').addEventListener('click',function(){
+    location.href = "https://www.linkedin.com/in/christopher-moon-55b076239/"
 });
