6a4271925cedbf21f732131036e3e334d5bb3370
Christopher Moon <77298032+chrismoon-code@users.noreply.github.com>
2023-02-16 00:06:51 +0000

    Added images, reformatting, added links

--- website.js
+++ website.js
@@ -3,7 +3,6 @@ const switcher = document.querySelector('.btn');
 switcher.addEventListener('click',function(){
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
-
 const classname = document.body.className;
 if(classname = "light-theme"){
    this.textContent = "Dark";
